2d2cec3e5632985d72971679b2ca7fc15edc3f1f
Ednan Jhony <ednanjhony@yahoo.com.br>
2020-12-07 07:41:39 -0300

    editing dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,29 +1,53 @@
 import React from 'react';
-import { FiArrowDown, FiSearch, FiShoppingCart, FiUser } from 'react-icons/fi';
+import { FiSearch, FiShoppingCart, FiUser } from 'react-icons/fi';
 
 import logoImg from '../../assets/khali.png';
 import main2 from '../../assets/main2.jpg';
 import main1 from '../../assets/main1.jpg';
+import description from '../../assets/description.jpg';
 
-import { Container, Header, Logo, List, Nav, Main } from './styles';
+import {
+  Container,
+  Header,
+  Logo,
+  HeaderWelcome,
+  Navigation,
+  List,
+  Nav,
+  Main,
+  Description,
+} from './styles';
 
 const Dashboard: React.FC = () => (
   <Container>
     <Header>
-      <List>
-        <li>
-          <a href="/">
-            Shop <FiArrowDown />
-          </a>
-        </li>
-        <li>
-          <a href="/">About</a>
-        </li>
-      </List>
-
       <Logo>
         <img src={logoImg} alt="KhalyStore" />
       </Logo>
+    </Header>
+
+    <HeaderWelcome>
+      <h1>Bem-Vindo a Khaly store</h1>
+    </HeaderWelcome>
+
+    <Navigation>
+      <List>
+        <li>
+          <a href="/">Lingeries</a>
+        </li>
+        <li>
+          <a href="/">Biquinis</a>
+        </li>
+        <li>
+          <a href="/">Semi-joias</a>
+        </li>
+        <li>
+          <a href="/">Bijuteria</a>
+        </li>
+        <li>
+          <a href="/">Folheado</a>
+        </li>
+      </List>
 
       <Nav>
         <li>
@@ -42,16 +66,28 @@ const Dashboard: React.FC = () => (
           </a>
         </li>
       </Nav>
-    </Header>
+    </Navigation>
 
     <Main>
       <div>
         <img src={main2} alt="" />
+        <button type="button">COMPRE AGORA</button>
       </div>
       <div>
         <img src={main1} alt="" />
+        <button type="button">COMPRE AGORA</button>
       </div>
     </Main>
+
+    <Description>
+      <div>
+        <h2>
+          Entregamos para todo o Brasil, Selecione seus produtos e calcule o
+          frete.
+        </h2>
+        <img src={description} alt="Description" />
+      </div>
+    </Description>
   </Container>
 );
 
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,19 +3,12 @@ import styled from 'styled-components';
 export const Container = styled.div``;
 
 export const Header = styled.header`
-  position: relative;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   background-color: #fff;
   width: 100%;
   height: 100px;
-  padding: 0 50px 0 50px;
-
-  a {
-    text-decoration: none;
-    color: #e57373;
-  }
 `;
 
 export const Logo = styled.div`
@@ -27,8 +20,40 @@ export const Logo = styled.div`
   }
 `;
 
+export const HeaderWelcome = styled.div`
+  display: flex;
+  height: 150px;
+  width: 100%;
+  background-color: #fce4ec;
+  justify-content: center;
+  align-items: center;
+
+  > h1 {
+    font-family: 'Dancing Script', cursive;
+    font-weight: 600;
+    color: #e57373;
+  }
+`;
+
+export const Navigation = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  height: 50px;
+  width: 100%;
+  padding-left: 30px;
+
+  a {
+    text-decoration: none;
+    color: #e57373;
+    margin-right: 30px;
+  }
+`;
+
 export const List = styled.div`
   display: flex;
+  padding: 0 5px;
 
   svg {
     height: 10px;
@@ -64,7 +89,60 @@ export const Main = styled.main`
       width: 350px;
       height: 350px;
       border-radius: 9px;
+      box-shadow: 2px 2px 2px #e57373;
       margin: 30px 40px 0 40px;
+    }
+
+    button {
+      background-color: #fff;
+      color: #e57373;
+      opacity: 0.3;
+      position: relative;
+      height: 40px;
+      width: 150px;
+      border-radius: 9px;
+      border: 0;
+      top: -30%;
+      right: -40%;
+
+      &:hover {
+        background-color: #fff;
+        opacity: 1;
+      }
+    }
+  }
+
+  @media (max-width: );
+`;
+
+export const Description = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  height: 500px;
+  width: 100%;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 200px;
+    width: 90%;
+    background-color: #fce4ec;
+
+    > h2 {
+      margin-left: 150px;
+      color: #e57373;
+      font-family: 'Dancing Script', cursive;
+      font-weight: 600;
+    }
+
+    > img {
+      height: 350px;
+      width: 350px;
+      border-radius: 9px;
+      box-shadow: 2px 2px 2px #e57373;
     }
   }
 `;
